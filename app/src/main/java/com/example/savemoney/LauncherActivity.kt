c8004d3bbca6877e6aedd0b469807753a3768d05
stasvitalich <stasvitalich1994@gmail.com>
2023-03-24 15:40:31 +0600

    Login fragment was created.

--- app/src/main/java/com/example/savemoney/LauncherActivity.kt
+++ app/src/main/java/com/example/savemoney/LauncherActivity.kt
@@ -1,12 +1,16 @@
 package com.example.savemoney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.savemoney.databinding.ActivityLauncherBinding
 import render.animations.*
 
 class LauncherActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT:Long=4000
     lateinit var binding: ActivityLauncherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,12 @@ class LauncherActivity : AppCompatActivity() {
             render.setAnimation(Zoom().In(binding.imageLogo))
             render.start()
         }, 500)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, ChooseActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
 
     }
 }
---
+++ app/src/main/java/com/example/savemoney/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.example.savemoney
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.savemoney.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
