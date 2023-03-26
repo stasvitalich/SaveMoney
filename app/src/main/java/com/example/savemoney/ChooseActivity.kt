778375d975c450e4984e96f1f5d913d2a86561e3
stasvitalich <stasvitalich1994@gmail.com>
2023-03-26 23:50:19 +0600

    ehaviour of system back button was changed.

--- app/src/main/java/com/example/savemoney/ChooseActivity.kt
+++ app/src/main/java/com/example/savemoney/ChooseActivity.kt
@@ -20,5 +20,4 @@ class ChooseActivity : AppCompatActivity() {
 
     }
 
-
 }
--- app/src/main/java/com/example/savemoney/screens/ChooseFragment2.kt
+++ app/src/main/java/com/example/savemoney/screens/ChooseFragment2.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.navigation.Navigation
 import com.example.savemoney.R
 import com.example.savemoney.databinding.FragmentChoose2Binding
@@ -22,11 +24,23 @@ class ChooseFragment2 : Fragment() {
         return binding.root
     }
 
+    // Close application if user pushes system back button
+    // from start destination fragment.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.singInButton.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_chooseFragment2_to_loginFragment)
         }
+
     }
 
       companion object {
