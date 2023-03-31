2505ceee93d99218d2189a62e2b494e954fd5db7
stasvitalich <stasvitalich1994@gmail.com>
2023-03-31 20:59:51 +0600

    current user was saved

--- app/src/main/java/com/example/savemoney/AppActivity.kt
+++ app/src/main/java/com/example/savemoney/AppActivity.kt
@@ -22,5 +22,4 @@ class AppActivity : AppCompatActivity() {
 
     }
 
-
 }
--- app/src/main/java/com/example/savemoney/LauncherActivity.kt
+++ app/src/main/java/com/example/savemoney/LauncherActivity.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.core.os.postDelayed
 import com.example.savemoney.databinding.ActivityLauncherBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import render.animations.*
 
 class LauncherActivity : AppCompatActivity() {
 
     private val SPLASH_TIME_OUT:Long=4000
+    private lateinit var auth: FirebaseAuth
     lateinit var binding: ActivityLauncherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +36,17 @@ class LauncherActivity : AppCompatActivity() {
             render.start()
         }, 500)
 
+        auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, ChooseActivity::class.java))
+            if (currentUser != null){
+                startActivity(Intent(this, AppActivity::class.java))
+            } else{
+                startActivity(Intent(this, ChooseActivity::class.java))
+            }
             finish()
+
         }, SPLASH_TIME_OUT)
 
     }
