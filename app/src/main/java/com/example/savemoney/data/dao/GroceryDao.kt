6d4388b824388597cb07e4e9baa06849b74977f8
stasvitalich <stasvitalich1994@gmail.com>
2023-04-11 15:49:15 +0600

    DAO for room was created.

--- app/src/main/java/com/example/savemoney/data/dao/GroceryDao.kt
+++ app/src/main/java/com/example/savemoney/data/dao/GroceryDao.kt
@@ -1,7 +1,25 @@
 package com.example.savemoney.data.dao
 
 import androidx.room.*
+import com.example.savemoney.data.entities.ProductEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GroceryDao {
+
+    @Insert
+    suspend fun insert(productEntity: ProductEntity)
+
+    @Update
+    suspend fun update(productEntity: ProductEntity)
+
+    @Delete
+    suspend fun delete(productEntity: ProductEntity)
+
+    @Query("SELECT * FROM Grocery_table")
+    fun fetchAllGroceries():Flow<List<ProductEntity>>
+
+    @Query("SELECT * FROM Grocery_table where id=:id")
+    fun fetchGroceriesById(id:Int):Flow<ProductEntity>
+
 }
